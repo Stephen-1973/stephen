bc7606368bfac07b3565405ec7df83072a499c59
MeherDeepak-2005 <meherdeepakpc@gmail.com>
2021-10-14 22:11:58 +0530

    Loading Bug Fix

--- src/utils/Loading.js
+++ src/utils/Loading.js
@@ -3,7 +3,7 @@ import Typical from 'react-typical'
 function Loading() {
     return (
         <div className="loading">
-            <h1 className='loading_text'>
+            <h1 style={{ color: 'white'}} className='loading_text'>
                 <Typical wrapper='p' steps={['Loading...']}/>
             </h1>
         </div>
